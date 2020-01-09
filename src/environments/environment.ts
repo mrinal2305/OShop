f9f689857b78196d952a1f530fdcf6d4e330c193
Mrinal Kumar Karn <45876327+mrinal2305@users.noreply.github.com>
2020-01-10 02:30:50 +0530

    removed app id and all vulnerable contents

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
  production : true,
   firebase : {
-    apiKey: "AIzaSyDFGG1mls_NTLKbNjPt5qZ-bybB2i83tPw",
-    authDomain: "oshop-746be.firebaseapp.com",
-    databaseURL: "https://oshop-746be.firebaseio.com",
-    projectId: "oshop-746be",
-    storageBucket: "oshop-746be.appspot.com",
-    messagingSenderId: "424121128721",
-    appId: "1:424121128721:web:19531de619679f3e"
+    apiKey: "***********************",
+    authDomain: "****************",
+    databaseURL: "**********************",
+    projectId: "***********",
+    storageBucket: "***************",
+    messagingSenderId: "*******************",
+    appId: "*************************"
   }
 };
 
